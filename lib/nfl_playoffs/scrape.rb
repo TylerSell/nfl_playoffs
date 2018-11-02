c189c5347f7c8745106e73e2283250ca7c9a0a05
tylerselldesign <tylerselldesign@gmail.com>
2018-11-02 04:39:43 +0000

    building out the scrape class

--- lib/nfl_playoffs/scrape.rb
+++ lib/nfl_playoffs/scrape.rb
@@ -1,3 +1,36 @@
 class Scrape
-  attr_accessor :team, :record
+  attr_accessor :team, :wins, :losses, :ties 
+  
+  def self.scrape_afc
+    afc = []
+  end
+  
+  def self.scrape_nfc
+    nfc = []
+  end
+  
+  def afc_playoff_teams
+    
+  end
+  
+  def nfc_playoff_teams
+    
+  end
+  
+  def afc_bye
+    
+  end
+  
+  def nfc_bye
+    
+  end
+  
+  def afc_home_field
+    
+  end
+  
+  def nfc_home_field
+    
+  end
+  
 end
