50a29422477082c8b2f9c58c049ffc84316f04b6
tylerselldesign <tylerselldesign@gmail.com>
2018-11-07 06:23:41 +0000

    afc scrape base

--- lib/nfl_playoffs/afc.rb
+++ lib/nfl_playoffs/afc.rb
@@ -6,7 +6,17 @@ class AFC
   @@all = []
   
   def self.scrape_afc
+    doc = Nokogiri::HTML(open(https://www.si.com/nfl/standings/playoff))
     
+    team = self.new 
+    team.city = 
+    team.name = 
+    team.combined = 
+    team.wins = 
+    team.losses = 
+    team.ties = 
+    
+    @@all << team 
   end
   
   def self.all 
