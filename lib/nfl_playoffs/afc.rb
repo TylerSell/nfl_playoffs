16d5bd5e36bc60dcf8021e79523d7ef871163f21
tylerselldesign <tylerselldesign@gmail.com>
2018-11-09 21:34:09 +0000

    update scrape for initialize

--- lib/nfl_playoffs/afc.rb
+++ lib/nfl_playoffs/afc.rb
@@ -9,18 +9,18 @@ class AFC
     doc = Nokogiri::HTML(open("https://www.si.com/nfl/standings/playoff"))
     teams_table = doc.at_css("table:nth-child(2)")
     rows = teams_table.at_css("tr")
+    rows = rows.select { |row| row.css!(".table-heading") }
     
     teams = rows.each do |row|
-      team = self.new
-      team.city = row.css("td.team-column div.collapse-narrow--inline-block div.team-city a").text 
-      team.name = row.css("td.team-column").css("div.collapse-narrow--inline-block").css("div.team-name a").text 
-      team.combined = "#{team.city} #{team.name}"
-      team.wins = row.css("td.numeric-score wins").text.strip
-      team.losses = row.css("td.numeric-score losses").text.strip
-      team.ties = row.css("td.numeric-score draws").text.strip
+      team_city = row.css("td.team-column div.collapse-narrow--inline-block div.team-city a").text 
+      team_name = row.css("td.team-column").css("div.collapse-narrow--inline-block").css("div.team-name a").text 
+      combined = "#{team.city} #{team.name}"
+      wins = row.css("td.numeric-score wins").text.strip
+      losses = row.css("td.numeric-score losses").text.strip
+      ties = row.css("td.numeric-score draws").text.strip
+      AFC.new(combined, wins, losses, ties)
     end
     
-    @@all << teams
   end
   
   def self.all 
