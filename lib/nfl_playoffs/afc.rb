a305ff51e8d1d442e4a332571696e570166c7090
tylerselldesign <tylerselldesign@gmail.com>
2018-11-09 00:02:58 +0000

    more testing

--- lib/nfl_playoffs/afc.rb
+++ lib/nfl_playoffs/afc.rb
@@ -6,20 +6,20 @@ class AFC
   @@all = []
   
   def self.scrape_afc
-    doc = Nokogiri::HTML(open(https://www.si.com/nfl/standings/playoff))
+    doc = Nokogiri::HTML(open("https://www.si.com/nfl/standings/playoff"))
     teams_table = doc.at_css("table:nth-child(2)")
     rows = teams_table.at_css("tr")
-    rows = rows.at_css(".table-heading").remove
     
     team = self.new
     
     rows.each do |row|
-      team.city = row.at_css(".team-city a").text 
-      team.name = row.at_css(".team-name a").text 
+      team.city = row.css(".team-city a").text 
+      team.name = row.css(".team-name a").text 
       team.combined = "#{team.city} #{team.name}"
-      team.wins = row.at_css(".numeric-score wins").text 
-      team.losses = row.at_css(".numeric-score losses").text
-      team.ties = row.at_css(".numeric-score draws").text
+      team.wins = row.css(".numeric-score wins").text 
+      team.losses = row.css(".numeric-score losses").text
+      team.ties = row.css(".numeric-score draws").text
+    end
     
     @@all << team 
   end
