86c07f166febe7da250390f904739dab35bcd9af
tylerselldesign <tylerselldesign@gmail.com>
2018-12-01 21:50:47 +0000

    Change CLI for extra levels of information.

--- lib/nfl_playoffs/cli.rb
+++ lib/nfl_playoffs/cli.rb
@@ -58,29 +58,44 @@ class CLI
         end
         menu
       when "2"
-        NFC.playoff_teams
+        AFC.standings
+        puts ""
+        puts "Would you like more information? Y/N"
+        input = gets.strip.downcase
+        if input == "y"
+          puts <<~HEREDOC
+            1. Playoff Teams  
+            2. Home Field Advantage
+            3. First Round Bye Teams
+            4. Wild Card Teams 
+            5. Main Menu 
+          HEREDOC
+          input = gets.strip
+          case input 
+            when "1"
+              NFC.playoff_teams
+              menu 
+            when "2"
+              NFC.home_field
+              menu 
+            when "3"
+              NFC.bye_teams
+              menu 
+            when "4"
+              NFC.wild_card
+              menu 
+            when "5"
+              menu
+          end
+        elsif input == "n"
+          menu 
+        else 
+          puts "I don't understand."
+        end
         menu
       when "3"
-        AFC.bye_teams
-        menu
-      when "4"
-        AFC.home_field
-        menu
-      when "5"
-        NFC.bye_teams
-        menu
-      when "6"
-        NFC.home_field
-        menu
-      when "7"
-        AFC.wild_card
-        menu 
-      when "8"
-        NFC.wild_card
-        menu 
-      when "9"
         goodbye
-        exit 
+        exit
       else 
         puts "I don't understand please choose again."
         puts ""
