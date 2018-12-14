9f34f443cb72212c36db105d550fcbe201449ec8
tylerselldesign <tylerselldesign@gmail.com>
2018-12-14 17:02:43 +0000

    cleaning up

--- lib/nfl_playoffs/scraper.rb
+++ lib/nfl_playoffs/scraper.rb
@@ -5,32 +5,12 @@ class Scraper
 
 
   def self.scrape_afc
-
     teams_table = self.doc.at_css("table:nth-child(2)")
-    # rows = teams_table.css("tr").each do |row|
-    #    team_city = row.css("div.team-city a").text
-    #    team_name = row.css("div.team-name a").text
-    #    combined = "#{team_city} #{team_name}"
-    #    wins = row.css("td.numeric-score.wins").text.strip
-    #    losses = row.css("td.numeric-score.losses").text.strip
-    #    ties = row.css("td.numeric-score.draws").text.strip
-    #    AFC.new(combined, wins, losses, ties)
-    # end
     self.rows(AFC, teams_table)
   end
 
   def self.scrape_nfc
-    # doc = Nokogiri::HTML(open("https://www.si.com/nfl/standings/playoff"))
     teams_table = @@doc.at_css("table:nth-child(6)")
-    # rows = teams_table.css("tr").each do |row|
-    #    team_city = row.css("div.team-city a").text
-    #    team_name = row.css("div.team-name a").text
-    #    combined = "#{team_city} #{team_name}"
-    #    wins = row.css("td.numeric-score.wins").text.strip
-    #    losses = row.css("td.numeric-score.losses").text.strip
-    #    ties = row.css("td.numeric-score.draws").text.strip
-    #    NFC.new(combined, wins, losses, ties)
-    #  end
     self.rows(NFC, teams_table)
   end
 
