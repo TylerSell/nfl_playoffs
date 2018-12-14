015692a900034cefd6f6ba62bdac3862d487b52f
tylerselldesign <tylerselldesign@gmail.com>
2018-12-14 17:00:50 +0000

    refactor and added class

--- lib/nfl_playoffs/cli.rb
+++ lib/nfl_playoffs/cli.rb
@@ -1,26 +1,28 @@
-class CLI 
-  
-  def call 
+class CLI
+
+  def call
     welcome
+    Scraper.scrape_afc
+    Scraper.scrape_nfc
     menu
   end
-  
+
   def welcome
     puts "If the season ended today..."
     puts ""
   end
-  
-  def menu 
+
+  def menu
     puts "Choose one of the following numbers:"
     puts ""
     puts <<~HEREDOC
-      1. AFC Standings 
+      1. AFC Standings
       2. NFC Standings
-      3. Exit 
+      3. Exit
     HEREDOC
-    
+
     input = gets.strip
-    case input 
+    case input
       when "1"
         puts ""
         puts "AFC Standings"
@@ -33,42 +35,42 @@ class CLI
         if input == "y"
           puts ""
           puts <<~HEREDOC
-            1. Playoff Teams  
+            1. Playoff Teams
             2. Home Field Advantage
             3. First Round Bye Teams
-            4. Wild Card Teams 
-            5. Main Menu 
+            4. Wild Card Teams
+            5. Main Menu
           HEREDOC
           puts ""
           input = gets.strip
-          case input 
+          case input
             when "1"
               puts ""
               AFC.playoff_teams
               puts ""
-              menu 
+              menu
             when "2"
               puts ""
               AFC.home_field
               puts ""
-              menu 
+              menu
             when "3"
               puts ""
               AFC.bye_teams
               puts ""
-              menu 
+              menu
             when "4"
               puts ""
               AFC.wild_card
               puts ""
-              menu 
+              menu
             when "5"
               puts ""
               menu
           end
         elsif input == "n"
-          menu 
-        else 
+          menu
+        else
           puts "I don't understand."
         end
         menu
@@ -84,55 +86,55 @@ class CLI
         if input == "y"
           puts ""
           puts <<~HEREDOC
-            1. Playoff Teams  
+            1. Playoff Teams
             2. Home Field Advantage
             3. First Round Bye Teams
-            4. Wild Card Teams 
-            5. Main Menu 
+            4. Wild Card Teams
+            5. Main Menu
           HEREDOC
           puts ""
           input = gets.strip
-          case input 
+          case input
             when "1"
               puts ""
               NFC.playoff_teams
               puts ""
-              menu 
+              menu
             when "2"
               puts ""
               NFC.home_field
               puts ""
-              menu 
+              menu
             when "3"
               puts ""
               NFC.bye_teams
               puts ""
-              menu 
+              menu
             when "4"
               puts ""
               NFC.wild_card
               puts ""
-              menu 
+              menu
             when "5"
               puts ""
               menu
           end
         elsif input == "n"
-          menu 
-        else 
+          menu
+        else
           puts "I don't understand."
         end
         menu
       when "3"
         goodbye
         exit
-      else 
+      else
         puts "I don't understand please choose again."
         puts ""
         menu
     end
   end
- 
+
   def goodbye
     puts ""
     puts "Goodbye. Check back next week for an update."
--- lib/nfl_playoffs/nfc.rb
+++ lib/nfl_playoffs/nfc.rb
@@ -1,34 +1,20 @@
 class NFC
   extend Display
-  
-  attr_accessor :name, :wins, :losses, :ties, :city, :combined 
-  
+
+  attr_accessor :name, :wins, :losses, :ties, :city, :combined
+
   def initialize(combined, wins, losses, ties)
     @combined = combined
     @wins = wins
     @losses = losses
     @ties = ties
-    @@all << self 
+    @@all << self
   end
-  
+
   @@all = []
-  
-  def self.scrape_nfc
-    doc = Nokogiri::HTML(open("https://www.si.com/nfl/standings/playoff"))
-    teams_table = doc.at_css("table:nth-child(6)")
-    rows = teams_table.css("tr").each do |row| 
-       team_city = row.css("div.team-city a").text 
-       team_name = row.css("div.team-name a").text 
-       combined = "#{team_city} #{team_name}"
-       wins = row.css("td.numeric-score.wins").text.strip
-       losses = row.css("td.numeric-score.losses").text.strip
-       ties = row.css("td.numeric-score.draws").text.strip
-       NFC.new(combined, wins, losses, ties)
-     end
-  end
-  
-  def self.all 
-    self.scrape_nfc
-    @@all 
+
+
+  def self.all
+    @@all
   end
 end
